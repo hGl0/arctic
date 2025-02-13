536cfc1e19908dcb712c6644eb3a29602583cab9
hGl0 <hanna.gloyna@gmx.de>
2025-02-13 16:20:24 +0100

    Error handling in plot_dendrogram added, kmeans added, evaluation of feature relevance by pca planed

--- src/arctic/__init__.py
+++ src/arctic/__init__.py
@@ -25,23 +25,35 @@ P0 = 1013.25
 def plot_dendrogram(model, **kwargs):
     # Create linkage matrix and then plot the dendrogram
     # create the counts of samples under each node
-    counts = np.zeros(model.children_.shape[0])
-    n_samples = len(model.labels_)
-    for i, merge in enumerate(model.children_):
-        current_count = 0
-        for child_idx in merge:
-            if child_idx < n_samples:
-                current_count += 1  # leaf node
-            else:
-                current_count += counts[child_idx - n_samples]
-        counts[i] = current_count
+    try:
+        counts = np.zeros(model.children_.shape[0])
+        n_samples = len(model.labels_)
+        for i, merge in enumerate(model.children_):
+            current_count = 0
+            for child_idx in merge:
+                if child_idx < n_samples:
+                    current_count += 1  # leaf node
+                else:
+                    current_count += counts[child_idx - n_samples]
+            counts[i] = current_count
 
-    linkage_matrix = np.column_stack(
-        [model.children_, model.distances_, counts]
-    ).astype(float)
+        linkage_matrix = np.column_stack(
+            [model.children_, model.distances_, counts]
+        ).astype(float)
 
-    # Plot the corresponding dendrogram
-    dendrogram(linkage_matrix, **kwargs)
+        # Plot the corresponding dendrogram
+        dendrogram(linkage_matrix, **kwargs)
+
+    except AttributeError as e:
+        print(f"AttributeError: {e}.\n Ensure 'model' has 'children_', 'distances_', and 'labels_' attributes.")
+    except TypeError as e:
+        print(f"TypeError: {e}.\n Check if model properties are of the correct data type.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+def pca_relevance():
+    pass
 
 
 def plot_correlation(df, cmap='coolwarm'):
