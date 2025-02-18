c93e6e1d0209c1e6f8e4bfbd750b63653a630b97
hGl0 <hanna.gloyna@gmx.de>
2025-02-18 17:56:58 +0100

    script solution added, feature importance by pca added, output for examples added

---
+++ Playground_script.py
@@ -0,0 +1,77 @@
+# This is an example script that processes input data
+# and plots graphs and insight into an output directory
+
+import argparse
+import pandas as pd
+import os
+import src.arctic as arctic
+
+
+# multiple arguments for other possible...!! First version!!
+def process_data(input_file, output_path, model, other):
+    """Main function to process data."""
+    # Read input CSV
+    try:
+        with open(input_file, "r", encoding="utf-8") as f:
+            filtered_lines = [line for line in f if line.startswith("D")]
+
+        # Convert filtered lines into DataFrame
+        from io import StringIO
+        df = pd.read_csv(
+            StringIO("".join(filtered_lines)),
+            delimiter=",",
+            low_memory=False
+        )
+
+        # To Do: include other parameters nicely
+        if other:
+            print('Successfully read input data')
+
+    except Exception as e:
+        print(f"Error reading CSV file: {e}")
+        return
+
+
+    # insert work flow later!
+    # To Do:
+    # csv with most important features by pca
+    # 2D/3D scatter plot with pca vectores
+    # correlation matrix as csv or png? both?
+
+    # spider plot with 6 most important features
+    # hierarchical: dendrogram
+    # kmeans: 2D/3D scatter with colored clusters? Geo map with colored clusters?
+
+    print(f'Magic data processing using {model}.')
+
+
+    # Save the processed file
+    processed_df = df
+    processed_df.to_csv(os.path.join(output_path, "processed_data.csv"), index=False)
+    print(f"Processed data saved to {output_path}")
+
+
+def main():
+    """Parse arguments and call processing function."""
+    parser = argparse.ArgumentParser(description="Process a CSV file using arctic.")
+    parser.add_argument("input", type=str, help="Path to input CSV file")
+    parser.add_argument("output_path", type=str, help="Path to save the processed output")
+    parser.add_argument("model", type=str, nargs='?',
+                        default="hierarchical", choices=['hierarchical', 'kmeans'],
+                        help="Clustering model: hierarchical (default), kmeans")
+    parser.add_argument('other', type=str, choices=['verbose'],
+                        nargs='?', default=None, help='Additional parameters')
+
+
+    args = parser.parse_args()
+
+    # Convert relative paths to absolute paths
+    input_file = os.path.abspath(args.input)
+    output_path = os.path.abspath(args.output_path)
+
+    # Call processing function
+    process_data(input_file, output_path, args.model, args.other)
+
+
+if __name__ == "__main__":
+    main()
--- src/arctic/__init__.py
+++ src/arctic/__init__.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.cluster.hierarchy import dendrogram
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 ##### Constants #####
 # 0°C in K
@@ -22,6 +24,8 @@ g = 9.80665
 P0 = 1013.25
 
 
+# plots a dendrogram for a given model with computed distances between samples
+# model requires to have children_ and distances_ attribute
 def plot_dendrogram(model, **kwargs):
     # Create linkage matrix and then plot the dendrogram
     # create the counts of samples under each node
@@ -44,6 +48,7 @@ def plot_dendrogram(model, **kwargs):
         # Plot the corresponding dendrogram
         dendrogram(linkage_matrix, **kwargs)
 
+    # Catch errors
     except AttributeError as e:
         print(f"AttributeError: {e}.\n Ensure 'model' has 'children_', 'distances_', and 'labels_' attributes.")
     except TypeError as e:
@@ -52,10 +57,136 @@ def plot_dendrogram(model, **kwargs):
         print(f"An unexpected error occurred: {e}")
 
 
-def pca_relevance():
-    pass
+# compute pca for a given data set
+# extract most important features as dataframe
+# if plot == True plot the components as vectors vs. the data set
+# with x and y being the 2 most important features
+def pca_importance(df, comp=4, **kwargs):
+    plot_type = kwargs.get('plot_type', '2D')
+    scaler = kwargs.get('scaler', StandardScaler)
+    savefig = kwargs.get('savefig', None)
+    savecsv = kwargs.get('savecsv', None)
+
+    # Scale data with StandardScaler x = (z-u)/s with u being the mean and s the standard deviation
+    if scaler:
+        try:
+            # Check if scaler is a class, not an instance
+            if isinstance(scaler, type):
+                scaler = scaler()
+            scaler.fit(df)
+            X = scaler.transform(df)
+        except TypeError as e:
+            print(f'Type Error: {e}. \n Ensure your dataframe has only numeric types.')
+            return None
+
+    # compute PCA
+    try:
+        pca = PCA()
+        pca.fit(X)
+        x_new = pca.transform(X)
+    except Exception as e:
+        print(f"Error while transforming X to PCA: {e}")
+        return None
+
+    # Init values for plotting
+    try:
+        score = x_new[:, :]
+        coeff = np.transpose(pca.components_[:, :])  # voher 0:comp, :
+
+        xs = score[:, 0]
+        ys = score[:, 1]
+        zs = score[:, 2]
+
+        n = coeff.shape[0]
+
+        scalex = 1.0 / (xs.max() - xs.min())
+        scaley = 1.0 / (ys.max() - ys.min())
+        scalez = 1.0 / (zs.max() - zs.min())
+
+        labels = df.columns.to_list() if hasattr(df, 'columns') else None
+        # color data by label
+        if 'label' in df.columns:
+            c = df['label'].to_list()
+        else:
+            c = None
+    except Exception as e:
+        print(f"Error while assigning values: {e}")
+        return None
+
+    # Plot data and principal components
+    # decision again match-case statement, might not work in older python versions (< 3.10)
+    try:
+        if plot_type == '2D':
+            if c:
+                plt.scatter(xs * scalex, ys * scaley, c=c, cmap='tab10')  #maybe useful with cluster labels?
+            else:
+                plt.scatter(xs * scalex, ys * scaley)
+            # Plot arrows of features
+            for i in range(n):
+                plt.arrow(0, 0, coeff[i, 0], coeff[i, 1], color='r', alpha=0.5)
+                if labels is None:
+                    plt.text(coeff[i, 0] * 1.15, coeff[i, 1] * 1.15,
+                             "Var" + str(i + 1),
+                             color='g', ha='center', va='center')
+                else:
+                    plt.text(coeff[i, 0] * 1.15, coeff[i, 1] * 1.15,
+                             labels[i],
+                             color='g', ha='center', va='center')
+
+            # make up for plot
+            plt.xlim(-1, 1)
+            plt.ylim(-1, 1)
+            plt.xlabel("PC{}".format(1))
+            plt.ylabel("PC{}".format(2))
+            plt.grid()
+            plt.title('2D Plot')
+            # save figure
+            if savefig: plt.savefig(savefig)
+            plt.show()
+
+        elif plot_type == '3D':
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            # Plot of pca
+            if c:
+                ax.scatter3D(xs * scalex, ys * scaley, zs * scalez, alpha=0.5, c=c, cmap='tab10')
+            else:
+                ax.scatter3D(xs * scalex, ys * scaley, zs * scalez, alpha=0.5)
+            # Plot arrows of features
+            for i in range(n):
+                ax.quiver(0, 0, 0, coeff[i, 0], coeff[i, 1], coeff[i, 2], color='r', alpha=0.5)
+                if labels is None:
+                    ax.text(coeff[i, 0] * 1.15, coeff[i, 1] * 1.15, coeff[i, 2]*1.15,
+                            'Var' + str(i + 1),
+                            color='g', ha='center', va='center')
+                else:
+                    ax.text(coeff[i, 0] * 1.15, coeff[i, 1] * 1.15, coeff[i,2]*1.15,
+                            labels[i],
+                            color='g', ha='center', va='center')
+            ax.set_xlim(-1, 1)
+            ax.set_ylim(-1, 1)
+            ax.set_zlim(-1, 1)
+            plt.title('3D Plot')
+            #save figure
+            if savefig: plt.savefig(savefig)
+            plt.show()
+        else:
+            print(f"Warning: Unkown plot type '{plot_type}'")
+    except Exception as e:
+        print(f"Error while plotting: {e}")
+
+    # generate overview of influencial features on pca
+    scores = pd.DataFrame(pca.components_[:comp].T,
+                          columns=[f'PC{i}'.format(i) for i in range(comp)],
+                          index=df.columns)
+    # store in csv
+    if savecsv: scores.to_csv(savecsv)
+    return scores
 
 
-def plot_correlation(df, cmap='coolwarm'):
+# compute correlation coefficient of a dataframe
+# recommended to use equally perceived colormaps with 0 as white
+# according to https://matplotlib.org/stable/users/explain/colors/colormaps.html
+def plot_correlation(df, cmap='RdBu'):
     correlation_matrix = df.corr(numeric_only=True)
     return correlation_matrix.style.background_gradient(cmap=cmap, vmin=-1, vmax=1.0)
